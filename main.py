ce0aeb5f263b4ad3515732399b7010059411a5ba
cormacflanagan <cormacflanagan12@gmail.com>
2023-09-09 23:27:12 +1000

    Split rubiksCube.py into a seperate file
    Began to work on the solving algorithem

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import random
+
 import matplotlib.pyplot as plt
 from rubiksCube import Rubik
 import pygame
 import sys
 from pygame.locals import *
+from copy import deepcopy
 
 pygame.init()
 # Set the dimensions of the display window
@@ -10,29 +13,77 @@ window_size = (1200, 600)
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Rubik's Cube Solver")
 
+def decision_tree(cube: Rubik, sides: list[str], counter=0, offset=3) -> list[tuple[str, int]]:
+    visited = []
+    result = []
+    goat = (cube.how_close(), (None, None), cube.state_id())
+    while True:
+        options = []
+
+        for side in sides:
+            for direction in (-1, 1):
+                cube.rotate(side, direction)
+                # print(cube.how_close())
+                options.append((cube.how_close(), (side, direction), cube.state_id()))
+                cube.rotate(side, -direction)
+
+        options.sort(key=lambda x: x[0])
+
+        if new_places := [
+            option for option in options if option[-1] not in visited
+        ]:
+            best = new_places[0]
+            # if best[0] > goat[0]+offset:
+            #     goat_pos = options.index(goat)
+            #     result = result[:goat_pos]
+            #     cube.reform(goat[-1])
+            #     print(f'{counter}: Jump back to {goat}')
+
+
+            cube.rotate(best[1][0], best[1][1])
+            print(f'{counter}: {best}')
+            if best[0] == 0:
+                result.append(best[1])
+                break
+
+            visited.append(best[-1])
+            result.append(best)
+
+        if best[0] < goat[0]:
+            goat: tuple[int, tuple[str, int], str] = min(options, key=lambda x: x[0])
+        counter += 1
+
+    return result
+
+
+
 
 def main():
     cube = Rubik(screen)
     # Use a breakpoint in the code line below to debug your script.
-    moves = cube.shuffle_cube(2000)
-    x = range(len(moves))
-    y = []
+    cube.update()
+    moves = cube.shuffle_cube(100)
+    moves.reverse()
+
+    move_num = range(len(moves))
+    value = []
     y2 = []
-    while moves:
 
-        # Clear the screen
-        move = moves.pop(-1)
-        cube.rotate(move[0], -move[1])
-        y.append(cube.how_close())
-        y2.append(len(moves)/(cube.how_close()+1))
+    moves = decision_tree(cube, ["Left", "Bottom", "Back"])
+
+    for move in moves:
+        cube.rotate(move[0], move[1])
+        value.append(cube.how_close())
         cube.update()
+    cube.update()
 
-    plt.plot(x, y, y2, marker='o', linestyle='-')
+    plt.plot(move_num, value, y2, marker='o', linestyle='-')
     plt.xlabel('Move')
     plt.ylabel('How Close to Solved')
     plt.title('Rubik\'s Cube Solver')
     plt.grid(True)
     plt.show()
+    print(sum(value) / len(value))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- rubiksCube.py
+++ rubiksCube.py
@@ -5,13 +5,13 @@ import random
 
 class Rubik:
     def __init__(self, screen : pygame.Surface | None):
-        self.Top: "Top is Red" = np.array(3*[3*['R']])
-        self.Bottom: "Bottom is Orange" = np.array(3*[3*['O']])
-        self.Front: "Front Is White" = np.array(3*[3*['W']])
-        self.Right: 'Right is Yellow' = np.array(3 * [3 * ['Y']])
-        self.Left: "Left is Blue" = np.array(3*[3*['B']])
-        self.Back: "Back is Green" = np.array(3*[3*['G']])
-        self.cube = [[self.Top, self.Bottom, self.Front, self.Right, self.Left, self.Back]]
+        self.Top: "Top is Red" = np.array([[f'R{i}{j}' for i in range(3)] for j in range(3)])
+        self.Bottom: "Bottom is Orange" = np.array([[f'O{i}{j}' for i in range(3)] for j in range(3)])
+        self.Front: "Front Is White" = np.array([[f'W{i}{j}' for i in range(3)] for j in range(3)])
+        self.Right: 'Right is Yellow' = np.array([[f'Y{i}{j}' for i in range(3)] for j in range(3)])
+        self.Left: "Left is Blue" = np.array([[f'B{i}{j}' for i in range(3)] for j in range(3)])
+        self.Back: "Back is Green" = np.array([[f'G{i}{j}' for i in range(3)] for j in range(3)])
+        self.cube: list[np.ndarray] = [self.Top, self.Bottom, self.Front, self.Right, self.Left, self.Back]
         self._screen = screen
         self._solved = copy.deepcopy(self.cube)
         self._colors_dict = {
@@ -29,7 +29,7 @@ class Rubik:
         if side == 'Top':
             self.Top = np.rot90(self.Top, direction)
             buffer = np.concatenate([self.Front[:, 0], self.Right[:, 0], self.Back[:, 2], self.Left[:, 2]])
-            moved = buffer = np.roll(buffer, 3*direction)
+            moved = np.roll(buffer, 3*direction)
             (
                 self.Front[:, 0],
                 self.Right[:, 0],
@@ -68,33 +68,41 @@ class Rubik:
             self.Top[:, 2], self.Right[2, :], self.Bottom[:, 0], self.Left[0, :] = np.split(buffer, 4)
 
         else:
-            print("Invalid side")
+            raise ValueError("Invalid side")
 
     def shuffle_cube(self, num_times):
         moves = []
         for _ in range(num_times):
             move = (random.choice(['Top', 'Bottom', 'Front', 'Right', 'Left', 'Back']), random.choice([1, -1]))
             self.rotate(move[0], move[1])
+            self.update()
+            pygame.time.wait(3)
             moves.append(move)
         return moves
 
     def draw_front(self, pos, color, shift=0):
+        color = color[0]
         # Draw the front face of the cube
         pygame.draw.rect(self._screen, self._colors_dict[color],
                          np.add((100 + shift, 250, 100, 100), pos + (0, 0)))
         # Add other faces of the cube here...
 
     def draw_side(self, pos, color, shift=0):
+        color = color[0]
         pygame.draw.polygon(self._screen, self._colors_dict[color],
                             np.add(((400 + shift, 350), (450 + shift, 300), (450 + shift, 200), (400 + shift, 250)),
                                    pos))
 
     def draw_top(self, pos, color, shift=0):
+        color = color[0]
         pygame.draw.polygon(self._screen, self._colors_dict[color],
                             np.add(((250 + shift, 200), (150 + shift, 200), (100 + shift, 250), (200 + shift, 250)),
                                    pos))
 
     def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
         self._screen.fill((0, 50, 0))
 
         for (x, y), col in np.ndenumerate(self.Front):
@@ -114,7 +122,30 @@ class Rubik:
 
         for (x, y), col in np.ndenumerate(self.Left):
             self.draw_side((x * 50, +y * 100 - 50 * x), col, 600)
+
         pygame.display.update()
-    def how_close(self):
-        return np.sum(np.equal(self.cube[0], self._solved[0]))
+    def how_close(self) -> int:
+        return 54-np.sum(np.equal(self.cube, self._solved))
+
+    def state_id(self) -> str:
+        return ':'.join(''.join(side.flatten('F')) for side in self.cube)
+
+    def reform(self, state_id: str):
+        # [self.Top, self.Bottom, self.Front, self.Right, self.Left, self.Back]
+        states = state_id.split(':')
+        self.Top = np.array(list(states[0])).reshape((3, 3), order='F')
+        self.Bottom = np.array(list(states[1])).reshape((3, 3), order='F')
+        self.Front = np.array(list(states[2])).reshape((3, 3), order='F')
+        self.Right = np.array(list(states[3])).reshape((3, 3), order='F')
+        self.Left = np.array(list(states[4])).reshape((3, 3), order='F')
+        self.Back = np.array(list(states[5])).reshape((3, 3), order='F')
+        np.rot90(self.Top, 2)
+        np.rot90(self.Bottom, 1)
+        np.rot90(self.Front, 2)
+        np.rot90(self.Right, 0)
+        np.rot90(self.Left, 1)
+        np.rot90(self.Back, 0)
+
+
+
 
